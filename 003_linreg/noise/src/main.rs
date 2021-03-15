b14f2456c1d832707da8be100a76adfedd1870f4
Axect <axect@outlook.kr>
2021-03-15 11:52:16 +0900

    Noise

---
+++ 003_linreg/noise/src/main.rs
@@ -0,0 +1,16 @@
+extern crate peroxide;
+use peroxide::fuga::*;
+
+fn main() {
+    let n = Normal(60, 20);
+    //let eps = Normal(0, 5);
+    //let y = n.sample(100).add_v(&eps.sample(100)).filter(|x| x <= 100f64);
+    let y = n.sample(100).filter(|x| x <= 100f64);
+    
+    let mut df = DataFrame::new(vec![]);
+    df.push("y", Series::new(y));
+
+    df.print();
+
+    df.write_nc("data.nc").expect("Can't write nc");
+}
